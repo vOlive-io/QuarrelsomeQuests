b2aaa8d91c5699b722b3b2b498465673b67795aa
vOlive2.0 <141564336+vOlive-io@users.noreply.github.com>
2026-01-07 11:04:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import { stats } from "data/player.js";
-import { store } from "data/store.js";
-import { classes } from "data/classes.js";
+import { stats } from "/data/player.js";
+import { store } from "/data/store.js";
+import { classes } from "/data/classes.js";
 
 ///////////////////////////
 //       TAB NAV         //
