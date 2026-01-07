293e34fd10d814c8bd9ea1bd627aea5af58f9428
vOlive2.0 <141564336+vOlive-io@users.noreply.github.com>
2026-01-07 09:34:10 -0700

    Update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+import "./data/classes.js";
+import "./data/player.js";
+import "./data/store.js";
 
 function openClass() { }
 function openExplore() { }
 function openStore() { }
-function openPrestige() { }
 function openStats() { }
