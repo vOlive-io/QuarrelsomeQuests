e44bc556b272fe27a7ac8692fd9af98b06358343
vOlive2.0 <141564336+vOlive-io@users.noreply.github.com>
2026-01-07 10:50:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ import { store } from "../data/store.js";
 import { classes } from "../data/classes.js";
 
 function openClass() { 
-    
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("classes").style.display = "block";
+    updateClassTab();
+}
+function updateClassTab() {
+    const classTab = document.getElementById("classes");
+    //to be implemented: display class selection and upgrades
 }
 function openExplore() { }
 function openStore() { }
