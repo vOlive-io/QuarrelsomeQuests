44d1ac3589d2d6be3e80007c0e02e4aa87f99cc4
vOlive2.0 <141564336+vOlive-io@users.noreply.github.com>
2026-01-07 09:43:58 -0700

    Update classes.js

--- data/classes.js
+++ data/classes.js
@@ -2,6 +2,14 @@ export const classes = [
     {
         name: "Warrior",
         selected: true,
+        stats : {
+            health: 150,
+            attack: 10,
+            defense: 8,
+            mana: 50,
+            luck: 5,
+            complexity: "Easy",
+        }
     }
 
 ];
