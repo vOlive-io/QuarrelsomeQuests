b73389497481f3835e202a976442f51e639e20e6
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2026-01-09 07:31:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,19 @@ import { classes } from "./data/classes.js";
 
 function openClass() { 
     document.getElementById("welcome").style.display = "none";
+    document.getElementById("x").style.display = "block";
     document.getElementById("classes").style.display = "block";
     updateClassTab();
 }
 function openStore() {
     document.getElementById("welcome").style.display = "none";
+    document.getElementById("x").style.display = "block";
     document.getElementById("store").style.display = "block";
     updateStoreTab();
 }
 function openStats() { 
     document.getElementById("welcome").style.display = "none";
+    document.getElementById("x").style.display = "block";
     document.getElementById("stats").style.display = "block";
     updateStatsTab();
 }
@@ -35,6 +38,7 @@ function openExplore() { }
 
 function closeMenu(menu) {
     document.getElementById(menu).style.display = "none";
+    document.getElementById("x").style.display = "none";
     document.getElementById("welcome").style.display = "block";
 }
 
