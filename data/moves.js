1d85b40ba156aa2ba1005ef73a7b5c3f89c93faa
vOlive2.0 <141564336+vOlive-io@users.noreply.github.com>
2026-01-13 11:26:28 -0700

    Update moves.js

---
+++ data/moves.js
@@ -0,0 +1,21 @@
+export const classes = [
+    {
+        name: "Sword Swing",
+        run: function() {
+            console.log("Executing Sword Swing move!");
+        }
+    },
+    {
+        name: "Sword Leap",
+    },
+    {
+        name: "Injured Stife",
+    },
+    {
+        name: "Power Strike",
+    },
+    {
+        name: "Whirlwind",
+    },
+];
+console.log("✅ Moves loaded:", classes);
