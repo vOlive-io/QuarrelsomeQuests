5c638b313b6f8f647306e37da9cef7ad195c5c11
vOlive2.0 <141564336+vOlive-io@users.noreply.github.com>
2026-01-15 11:17:07 -0700

    Update player.js

--- data/player.js
+++ data/player.js
@@ -1,7 +1,15 @@
 export const stats = [
     {
+        //extras to add on to class base stats
         extraHealth: 10,
+        extraAttack: 2, 
+        extraDefense: 2,
         extraMana: 5,
+        extraEnergy: 10,
+        extraSaturation: 5,
+        extraLuck: 2,
+        //player info
+        questsCompleted: 0,
         companion: {},
         achievementPoints: 0,
         gold: 0,
