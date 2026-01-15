aacc14e069ea2559a1ebc6f57699f2c5496f4a1b
vOlive2.0 <141564336+vOlive-io@users.noreply.github.com>
2026-01-15 11:23:34 -0700

    Update moves.js

--- data/moves.js
+++ data/moves.js
@@ -1,13 +1,16 @@
 export const moves = {
-    "Sword Swing": (player, target) => {
+    "Sword Swing": (player) => {
         if(player.energy < 10) {
             console.log(`${player.name} does not have enough energy to perform Sword Swing!`);
             return;
         } else {
             player.energy -= 10;
-            const damage = player.attack;
-            target.hp -= damage;
-            console.log(`${player.name} swings sword for ${damage} damage!`);
+            const buff = 1.0;
+            if(player.weaponBuff === "Sword") {buff += player.buff;}
+            if(player.potionBuff === "Attack") {buff += player.buff;}
+            if(player.companionBuffAttack == true) {buff += player.buff;}
+            const damage = player.attack * buff;
+            return damage;
         }
     },
 
@@ -17,7 +20,7 @@ export const moves = {
 
 
 
-    
+
     "Sword Leap": (player, target) => {
         const damage = player.attack;
         target.hp -= damage;
