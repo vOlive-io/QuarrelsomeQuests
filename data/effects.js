c1f2b7ab123efa8aad34d4266fe9cdc460a5d73b
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2026-01-19 14:12:54 -0700

    Update effects.js

---
+++ data/effects.js
@@ -0,0 +1,9 @@
+export const statuses = [
+    {
+        name: "Strength Buff",
+        type: "Attack",
+        description: "Increases physical strength by 20% for 5 minutes.",
+        duration: 300,
+    },
+];
+console.log("✅ Statuses are loaded:", statuses);
