77d34973084138f5099349bfda69c54f8cbbbf64
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2026-03-26 14:53:47 -0600

    fixed imports

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ import { stats } from "./data/stats.js";
 import { store } from "./data/store.js";
 //import { weapons } from "./data/weapons.js";
 //import { moves } from "./data/moves.js";
-//import { statuses } from "./data/effects.js";
+import { effects } from "./data/effects.js";
 
 ///////////////////////////
 //       TAB NAV         //
