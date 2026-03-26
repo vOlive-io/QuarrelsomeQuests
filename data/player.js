a9a995f53bd9cdebdcaac07a8c0bd60279f1852e
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2026-03-26 01:13:49 -0600

    Small

--- data/player.js
+++ data/player.js
@@ -1,18 +1,10 @@
 export const stats = [
     {
-        //extras to add on to class base stats
-        extraHealth: 10,
-        extraAttack: 2, 
-        extraDefense: 2,
-        extraMana: 5,
-        extraEnergy: 10,
-        extraSaturation: 5,
-        extraLuck: 2,
-        //player info
-        questsCompleted: 0,
-        companion: {},
-        achievementPoints: 0,
         gold: 0,
+        pets: {},
+        items: {},
+        quests: {},
+        achievements: {},
     }
 
 ];
