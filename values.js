e13ce6fe944f566793fc41658ac0263a2e5fc840
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2026-03-26 14:51:06 -0600

    Added the ranks

---
+++ values.js
@@ -0,0 +1,27 @@
+export const values = [
+    {
+        ranks: [
+            { title: "SSS+", value:  1000},
+            { title: "SSS",  value:  750},
+            { title: "SSS-", value:  500},
+            { title: "SS+",  value:  200},
+            { title: "SS",   value:  150 },
+            { title: "SS-",  value:  100 },
+            { title: "S+",   value:  75 },
+            { title: "S",    value:  60 },
+            { title: "S-",   value:  50},
+            { title: "A+",   value:  48 },
+            { title: "A",    value:  33 },
+            { title: "A-",   value:  25 },
+            { title: "B+",   value:  18 },
+            { title: "B",    value:  14 },
+            { title: "B-",   value: 10 },
+            { title: "C+",   value: 6 },
+            { title: "C",    value: 3 },
+            { title: "C-",    value:   1 },
+            { title: "Unranked",    value:   1 },
+        ],
+    }
+
+];
+console.log("✅ Values Stats are loaded:", values);
