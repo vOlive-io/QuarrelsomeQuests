34e2acdf69212a381ebbe517dd420eaebdb88a81
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2026-03-26 01:19:46 -0600

    Update pets.js

---
+++ data/pets.js
@@ -0,0 +1,31 @@
+export const pets = [
+    {
+        name: "",
+        selected: true,
+        unlocked: true,
+        class: "Warrior",
+        prestigeRequired: 100,
+    },
+    {
+        name: "",
+        selected: false,
+        unlocked: false,
+        class: "Mage",
+        prestigeRequired: 100,
+    },
+    {
+        name: "",
+        selected: false,
+        unlocked: false,
+        class: "Ranger",
+        prestigeRequired: 100,
+    },
+    {
+        name: "",
+        selected: false,
+        unlocked: false,
+        class: "Cleric",
+        prestigeRequired: 100,
+    },
+];
+console.log("✅ Pets loaded:", pets);
