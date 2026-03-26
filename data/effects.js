e3792699980664d7664a8a9b1b31586fef319c9e
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2026-03-26 01:13:57 -0600

    Update effects.js

--- data/effects.js
+++ data/effects.js
@@ -2,7 +2,7 @@ export const statuses = [
     {
         name: "Strength Buff",
         type: "Attack",
-        description: "Increases physical strength by 20% for 5 minutes.",
+        description: "Increases physical strength by 20% for 5 battles.",
         duration: 300,
     },
 ];
