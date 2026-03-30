72570634f0e80c2b819b97426209edfa11185c0a
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2026-03-30 10:50:48 -0600

    Refactor sword moves to use object structure

--- data/moves.js
+++ data/moves.js
@@ -1,62 +1,82 @@
 export const moves = {
-    "Sword Swing": (player) => {
-        if(player.energy < 10) {
-            console.log("You do not have enough energy to perform Sword Swing!");
-            return;
-        } else {
-            let attackBaseDamage = 1.0;
-            player.energy -= 10;
-            return (attackBaseDamage * player.attack) * calculateBuff(player, "Sword");   
+     "Sword Miss": {
+        baseDamage: 0,
+        specialText: (player) => {
+            player.stats.swordMissCount = (player.stats.swordMissCount || 0) + 1;
+            return `${player.ActiveCard.name} tries to swing their sword at ${player.ActiveEvil}, but MISSES!`;
         }
     },
-    "Sword Leap": (player) => {
-        if(player.energy < 15) {
-            console.log("You do not have enough energy to perform Sword Leap!");
-            return;
-        } else {
-            let attackBaseDamage = 1.2;
-            player.energy -= 15;
-            return (attackBaseDamage * player.attack) * calculateBuff(player, "Sword");   
+    "Sword Swing": {
+        baseDamage: 10,
+        specialText: (player) => {
+            player.stats.swordSwingCount = (player.stats.swordSwingCount || 0) + 1;
+            return `${player.ActiveCard.name} swings their sword at ${player.ActiveEvil}!`;
         }
     },
-    "Injured Stife": (player) => {
-        if((player.energy > 20 && player.energy > 10)|| player.health < player.maxHealth/10) {
-            console.log("You are not injured enough to perform Injured Strife!");
-            return;
-        } else {
-            let attackBaseDamage = 2;
-            player.energy -= 10;
-            return (attackBaseDamage * player.attack) * calculateBuff(player, "Sword");   
+    "Sword Strike": {
+        baseDamage: 10,
+        specialText: (player) => {
+            player.stats.swordStrikeCount = (player.stats.swordStrikeCount || 0) + 1;
+            return `${player.ActiveCard.name} strikes ${player.ActiveEvil} with their sword!`;
         }
     },
-    "Power Strike": (player) => {
-        if(player.energy < 20) {
-            console.log("You do not have enough energy to perform Power Strike!");
-            return;
-        } else {
-            let attackBaseDamage = 2.5;
-            player.energy -= 20;
-            return (attackBaseDamage * player.attack) * calculateBuff(player, "Sword");   
+    "Sword Leap": {
+        baseDamage: 15,
+        specialText: (player) => {
+            player.stats.swordLeapCount = (player.stats.swordLeapCount || 0) + 1;
+            return `${player.ActiveCard.name} leaps into the air and strikes ${player.ActiveEvil} with their sword!`;
         }
     },
-     "Whirlwind": (player) => {
-        if(player.energy < 25) {
-            console.log("You do not have enough energy to perform Whirlwind!");
-            return;
-        } else {
-            let attackBaseDamage = 3;
-            player.energy -= 25;
-            return (attackBaseDamage * player.attack) * calculateBuff(player, "Sword");   
+    "Sword Charge": {
+        baseDamage: 15,
+        specialText: (player) => {
+            player.stats.swordChargeCount = (player.stats.swordChargeCount || 0) + 1;
+            return `${player.ActiveCard.name} charges forward, hitting ${player.ActiveEvil} with their sword!`;
         }
     },
-};
-
-function calculateBuff(player, weapon) {
-    let buff = 1.0;
-    if(player.weaponBuff === weapon) {buff += player.buff;}   
-    if(player.potionBuff === "Attack") {buff += player.buff;}
-    if(player.companionBuffAttack) {buff += player.buff;}
-    return buff;
+    "Sword Spin": {
+        baseDamage: 15,
+        specialText: (player) => {
+            player.stats.swordSpinCount = (player.stats.swordSpinCount || 0) + 1;
+            return `${player.ActiveCard.name} spins around with thier sword, striking ${player.ActiveEvil}!`;
+        }
+    },
+    "Fake Out (Sword)": {
+        baseDamage: 20,
+        specialText: (player) => {
+            player.stats.swordFakeOutCount = (player.stats.swordFakeOutCount || 0) + 1;
+            let bodyParts = ["arm", "guts", "butt", "leg", "foot", "hand", "toe"];
+            let bodyPart = bodyParts[Math.floor(Math.random() * bodyParts.length)];
+            return `${player.ActiveCard.name} fakes out ${player.ActiveEvil}, and then stabs them in the ${bodyPart}!`;
+        }
+    },
+    "Sword Block": {
+        baseDamage: 20,
+        specialText: (player) => {
+            player.stats.swordBlockCount = (player.stats.swordBlockCount || 0) + 1;
+            let bodyParts = ["left arm", "right arm", "left leg", "right leg", "butt", "hand", "toe", "nose", "hair", "fingers"];
+            let bodyPart = bodyParts[Math.floor(Math.random() * bodyParts.length)];
+            return `{player.ActiveEvil} tries to attack ${player.ActiveCard.name}, but ${player.ActiveCard.name} blocks them and chops off their ${bodyPart}!`;
+        }
+    },
+    "Sword Impale": {
+        baseDamage: 25,
+        specialText: (player) => {
+            player.stats.swordImpaleCount = (player.stats.swordImpaleCount || 0) + 1;
+            let bodyParts = ["arm", "guts", "butt", "leg", "foot", "hand", "toe"];
+            let bodyPart = bodyParts[Math.floor(Math.random() * bodyParts.length)];
+            return `${player.ActiveCard.name} impales ${player.ActiveEvil} in the ${bodyPart}!`;
+        }
+    },
+    "Sword Flury": {
+        baseDamage: 30,
+        specialText: (player) => {
+            player.stats.swordFluryCount = (player.stats.swordFluryCount || 0) + 1;
+            let bodyParts = ["left arm", "right arm", "left leg", "right leg", "butt", "hand", "toe", "nose", "hair", "fingers"];
+            let bodyPart = bodyParts[Math.floor(Math.random() * bodyParts.length)];
+            return `${player.ActiveCard.name} swings their sword in a flury of rage, chopping off ${player.ActiveEvil}'s ${bodyPart}!`;
+        }
+    },
 }
 
-console.log("✅ Moves loaded:");
+console.log("✅ Moves Loaded");
