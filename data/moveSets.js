a384cc37b4ae64d5c8a4bcbf1752cf830f27400a
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2026-03-30 10:54:56 -0600

    Initialize move sets for new character types
    Added empty move sets for various character types.

--- data/moveSets.js
+++ data/moveSets.js
@@ -1,5 +1,13 @@
 export const moveSets = {
   knight: ["Sword Swing", "Sword Strike", "Sword Charge", "Sword Block", "Sword Impale"],
+  noble: [],
+  royal: [],
+  scrappyFighter: [],
+  dirtyFighter: [],
+  crossbow: [],
+  bow: [],
+  potion: [],
+  sneak: [],
 }
 
 console.log("✅ Move Sets Loaded");
