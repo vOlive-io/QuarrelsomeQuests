954d7f2117c9b6e20bcce87f18db8c78f607897d
vOlive2.0 <141564336+vOlive-io@users.noreply.github.com>
2026-04-01 11:31:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,8 @@ import { classes } from "./data/classes.js";
 import { stats } from "./data/player.js";
 import { store } from "./data/store.js";
 import { effects } from "./data/effects.js";
-//import { weapons } from "./data/weapons.js";
-//import { moves } from "./data/moves.js";
-
-console.log("VALUES:", values);
-console.log("RANKS:", values.ranks);
+import { moves } from "./data/moves.js";
+import { moveSets } from "./data/moves.js";
 
 ///////////////////////////
 //       PLAYER DEF      //
@@ -20,7 +17,6 @@ console.log("RANKS:", values.ranks);
 var rankBarWidth = 0;
 var userInfo = {
     playerName: "",
-    chosenClass: null,
 }
 var player = {
     xp: 1,
@@ -35,40 +31,10 @@ var player = {
     saturation: 0,
     luck: 0,
 };
-function resetPlayerStats() {
-    const chosenClass = getClassStats(userInfo.chosenClass);
-    player.maxHealth = stats.extraHealth+chosenClass.stats.health;
-    player.health = player.maxHealth;
-    player.attack = stats.extraAttack+chosenClass.stats.attack;
-    player.defense = stats.extraDefense+chosenClass.stats.defense;
-    player.mana = stats.extraMana+chosenClass.stats.mana;
-    player.energy = stats.extraEnergy+chosenClass.stats.energy;
-    player.saturation = stats.extraSaturation+chosenClass.stats.saturation;
-    player.luck = stats.extraLuck+chosenClass.stats.luck;
-}
-function updatePlayerStats() {
-    const chosenClass = getClassStats(userInfo.chosenClass);
-    player.maxHealth = stats.extraHealth+chosenClass.stats.health;
-    player.attack = stats.extraAttack+chosenClass.stats.attack;
-    player.defense = stats.extraDefense+chosenClass.stats.defense;
-    player.mana = stats.extraMana+chosenClass.stats.mana;
-    player.energy = stats.extraEnergy+chosenClass.stats.energy;
-    player.saturation = stats.extraSaturation+chosenClass.stats.saturation;
-    player.luck = stats.extraLuck+chosenClass.stats.luck;
-}
-
 
 ///////////////////////////
 //   HELPER FUNCTIONS    //
 ///////////////////////////
-function getClassStats(className) {
-    for (let i = 0; i < classes.length; i++) {
-        if (classes[i].name === className) {
-            return classes[i].stats;
-        }
-    }
-}
-
 function setXp(amount) {
     player.xp = amount;
 }
@@ -116,8 +82,6 @@ function getRankTitle() {
     console.log(title);
     return title;
 }
-
-
 
 //////////////////////////////////
 //    RANK DISPLAY FUNCTIONS    //
@@ -171,7 +135,6 @@ function updateRankBar() {
         rankDisplay.innerHTML = Math.floor(rankBarWidth*100) / 100+ '%';
     }
 }
-
 Object.assign(window, { setXp,
                         addXp,
                         removeXp,
