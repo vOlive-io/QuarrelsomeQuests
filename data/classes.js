73161d95d2b023d7605fa1dd02d2386b323663ea
vOlive2.0 <141564336+vOlive-io@users.noreply.github.com>
2026-04-01 11:52:00 -0600

    Update classes.js

--- data/classes.js
+++ data/classes.js
@@ -4,45 +4,25 @@ export const classes = {
         path: ["Warrior", "Brute", "Berserker"],
         selected: true,
         unlocked: true,
-        stats : {
-            health: 150,
-            attack: 10,
-            defense: 5,
-            mana: 50,
-        }
+        attack: 1.5,
     },
     "Mage": {
         path: ["Mage", "Sorcerer", "Archmage"],
         selected: false,
         unlocked: true,
-        stats : {
-            health: 100,
-            attack: 7,
-            defense: 8,
-            mana: 250,
-        }
+        attack: 1.5,
     },
     "Ranger": {
         path: ["Ranger", "Sharpshooter", "Stringer"],
         selected: false,
         unlocked: true,
-        stats : {
-            health: 80,
-            attack: 9,
-            defense: 6,
-            mana: 100,
-        },
+        attack: 1.5,
     },
     "Cleric": {
         path: ["Cleric", "Priest", "Crusader"],
         selected: false,
         unlocked: false,
-        stats : {
-            health: 200,
-            attack: 5,
-            defense: 10,
-            mana: 150,
-        },
+        attack: 1.5,
     }
 };
 console.log("✅ Classes loaded");
